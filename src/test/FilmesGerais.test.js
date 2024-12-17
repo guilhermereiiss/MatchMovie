6bfc063f4b8eb9ff34f0cf051ae0685feebd590a
guilhermereiiss <guiprogramador06@gmail.com>
2024-12-17 20:14:52 -0300

    projeto terminado

--- src/test/FilmesGerais.test.js
+++ src/test/FilmesGerais.test.js
@@ -1,7 +1,8 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import FilmesGerais from "./src/Filmes/Filmes";
+
+import FilmesGerais from "../Filmes/Filmes";
 import axios from "axios";
-import { adicionarFavorito, removerFavorito, obterFavoritos } from "./src/components/Favoritar/FavoritarFunc";
+import { adicionarFavorito, removerFavorito, obterFavoritos } from "../components/Favoritar/FavoritarFunc";
 
 jest.mock("axios");
 jest.mock("../components/Favoritar/FavoritarFunc", () => ({
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/MatchMovie/",
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    include: ['*/.{test,spec}.{js,mjs,cjs,jsx}']
+  
+  }
 })
